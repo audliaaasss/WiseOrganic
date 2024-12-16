89c04729268abd54fad202bef9450ca89a0f277e
audliaaasss <114149501+audliaaasss@users.noreply.github.com>
2024-12-17 01:43:14 +0700

    Second Commit

--- app/src/main/java/com/dicoding/wiseorganic/ui/main/analysis/AnalysisFragment.kt
+++ app/src/main/java/com/dicoding/wiseorganic/ui/main/analysis/AnalysisFragment.kt
@@ -1,60 +1,41 @@
 package com.dicoding.wiseorganic.ui.main.analysis
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.wiseorganic.R
+import com.dicoding.wiseorganic.databinding.FragmentAnalysisBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AnalysisFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AnalysisFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentAnalysisBinding
+    private lateinit var viewModel: AnalysisViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentAnalysisBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_analysis, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(AnalysisViewModel::class.java)
+
+        loadFirstChart()
+        loadSecondChart()
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AnalysisFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AnalysisFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun loadFirstChart() {
+        val chartBitmap = BitmapFactory.decodeResource(resources, R.drawable.first_chart)
+        binding.firstChartImageView.setImageBitmap(chartBitmap)
+    }
+
+    private fun loadSecondChart() {
+        val chartBitmap = BitmapFactory.decodeResource(resources, R.drawable.second_chart)
+        binding.secondChartImageView.setImageBitmap(chartBitmap)
     }
 }
---
+++ app/src/main/java/com/dicoding/wiseorganic/ui/main/analysis/AnalysisViewModel.kt
@@ -0,0 +1,9 @@
+package com.dicoding.wiseorganic.ui.main.analysis
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+
+class AnalysisViewModel : ViewModel() {
+    var firstChartBitmap: Bitmap? = null
+    var secondChartBitmap: Bitmap? = null
+}
